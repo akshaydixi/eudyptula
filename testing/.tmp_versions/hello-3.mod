d767d9be01c42f192158c4a435b2e5f0fb6eb1a4
Akshay Dixit <akshaydixi@gmail.com>
2014-04-24 02:11:29 +0530

    testing

---
+++ testing/.tmp_versions/hello-3.mod
@@ -0,0 +1,2 @@
+/home/akshay/yetAnotherWorkspace/eudyptula/testing/hello-3.ko
+/home/akshay/yetAnotherWorkspace/eudyptula/testing/hello-3.o
---
+++ testing/hello-3.c
@@ -0,0 +1,17 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+
+static int  hello3_data __initdata = 3;
+static int __init hello3_init(void){
+	printk(KERN_INFO "Testing kernel message bus: %d\n ",hello3_data);
+	return 0;	
+}
+
+static void  __exit hello3_exit(void){
+	printk(KERN_INFO "Exit message bus\n");
+}
+
+module_init(hello3_init);
+module_exit(hello3_exit);
+
